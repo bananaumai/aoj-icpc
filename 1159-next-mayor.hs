54c2622718f76491d87964c994a4fe94f27f0848
Yuta Shimakawa <banana.umai@gmail.com>
2017-01-21 22:48:30 +0900

    AOJ ICPC Next Mayor

---
+++ 1159-next-mayor.hs
@@ -0,0 +1,32 @@
+-- http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=1159
+import Control.Monad
+import Control.Applicative
+import Data.Foldable
+import Data.Sequence
+
+main :: IO ()
+main = do
+  [n, p] <- map read . words <$> getLine :: IO [Int]
+  unless (n == 0 && p == 0) $ do
+    print $ solve (candidates n) p
+    main
+
+data Candidate = Candidate { getN :: Int, getS :: Int } deriving Show
+type Candidates = Seq Candidate
+type Cup = Int
+
+candidates :: Int -> Candidates
+candidates n = fromList $ map (\n -> Candidate n 0) [0..n-1]
+
+updateCandidate :: Candidate -> Cup -> Candidate
+updateCandidate cdt 0 = Candidate (getN cdt) 0
+updateCandidate cdt _ = Candidate (getN cdt) (getS cdt + 1)
+
+solve :: Candidates -> Cup -> Int
+solve cdts cup = if check then getN currentCdt else solve nextCdts currentCup where
+  c :< restCdts = viewl cdts
+  currentCdt = updateCandidate c cup
+  currentCup = if cup == 0 then getS c else cup - 1
+  check = Data.Foldable.all (\c -> getS c == 0) (toList restCdts) && currentCup == 0 -- GHC7系
+  -- check = all (\c -> getS c == 0) restCdts && currentCup == 0 -- GHC8系
+  nextCdts = restCdts |> currentCdt
